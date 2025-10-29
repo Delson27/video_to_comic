f36408e78c41ab2220519cd57480e6e60306ea53
Delson <delsondsouza2004@gmail.com>
2025-10-29 10:48:30 +0530

    Reverted bubble placement

--- output_template/page.js
+++ output_template/page.js
@@ -1,236 +1,114 @@
 var pages = [
-    {
-        "panels": [
-            {
-                "image": "frame001",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame002",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame003",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame004",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame005",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame006",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame007",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame008",
-                "row_span": 1,
-                "col_span": 1
-            }
-        ],
-        "bubbles": [
-            {
-                "dialog": "The strength of the ancestors is here.",
-                "emotion": "normal",
-                "bubble_offset_x": 44.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 44.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "((action-scene))",
-                "emotion": "normal",
-                "bubble_offset_x": 59.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 59.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "The strength of the ancestors is here.",
-                "emotion": "normal",
-                "bubble_offset_x": 44.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 44.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "You cannot live like this baby in hate.",
-                "emotion": "jagged",
-                "bubble_offset_x": 44.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 44.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "If there is something you can do, then you must do it.",
-                "emotion": "normal",
-                "bubble_offset_x": 44.375,
-                "bubble_offset_y": 287.6666666666667,
-                "bubble_x": 44.375,
-                "bubble_y": 287.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "The children.",
-                "emotion": "normal",
-                "bubble_offset_x": 59.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 59.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": 433.001953125,
-                "lip_y": 218.66666666666666,
-                "tail_deg": -12.52468494631289,
-                "tail_offset_x": 78.09621336466626,
-                "tail_offset_y": -17.348817196008607
-            },
-            {
-                "dialog": "No, no, no, Spider.",
-                "emotion": "normal",
-                "bubble_offset_x": 59.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 59.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "Then we will find another way.",
-                "emotion": "normal",
-                "bubble_offset_x": 44.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 44.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            }
-        ]
-    },
-    {
-        "panels": [
-            {
-                "image": "frame009",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame010",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame011",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame012",
-                "row_span": 1,
-                "col_span": 1
-            },
-            {
-                "image": "frame013",
-                "row_span": 1,
-                "col_span": 1
-            }
-        ],
-        "bubbles": [
-            {
-                "dialog": "((action-scene))",
-                "emotion": "normal",
-                "bubble_offset_x": 59.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 59.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "((action-scene))",
-                "emotion": "normal",
-                "bubble_offset_x": 59.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 59.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "((action-scene))",
-                "emotion": "normal",
-                "bubble_offset_x": 59.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 59.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "((action-scene))",
-                "emotion": "normal",
-                "bubble_offset_x": 59.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 59.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            },
-            {
-                "dialog": "Your goddess has no dominion here.",
-                "emotion": "normal",
-                "bubble_offset_x": 44.375,
-                "bubble_offset_y": 301.6666666666667,
-                "bubble_x": 44.375,
-                "bubble_y": 301.6666666666667,
-                "lip_x": -1,
-                "lip_y": -1,
-                "tail_offset_x": null,
-                "tail_offset_y": null
-            }
-        ]
-    }
-]
+  {
+    panels: [
+      {
+        image: "frame001",
+        row_span: 1,
+        col_span: 1,
+      },
+      {
+        image: "frame002",
+        row_span: 1,
+        col_span: 1,
+      },
+      {
+        image: "frame003",
+        row_span: 1,
+        col_span: 1,
+      },
+      {
+        image: "frame004",
+        row_span: 1,
+        col_span: 1,
+      },
+      {
+        image: "frame005",
+        row_span: 1,
+        col_span: 1,
+      },
+      {
+        image: "frame006",
+        row_span: 1,
+        col_span: 1,
+      },
+    ],
+    bubbles: [
+      {
+        dialog: "So, what's new, Mark?",
+        emotion: "normal",
+        bubble_offset_x: 59.375,
+        bubble_offset_y: 301.6666666666667,
+        bubble_x: 59.375,
+        bubble_y: 301.6666666666667,
+        lip_x: 148.20422535211267,
+        lip_y: 212.19135802469134,
+        tail_deg: -45.20760932369475,
+        tail_offset_x: 56.36319738040835,
+        tail_offset_y: -56.773144893137015,
+      },
+      {
+        dialog: "How is your new job going?",
+        emotion: "normal",
+        bubble_offset_x: 59.375,
+        bubble_offset_y: 301.6666666666667,
+        bubble_x: 59.375,
+        bubble_y: 301.6666666666667,
+        lip_x: 162.7816901408451,
+        lip_y: 206.25,
+        tail_deg: -42.698718956324875,
+        tail_offset_x: 58.794380604141566,
+        tail_offset_y: -54.251459052963185,
+      },
+      {
+        dialog: "To be honest, I can't complain.",
+        emotion: "normal",
+        bubble_offset_x: 44.375,
+        bubble_offset_y: 301.6666666666667,
+        bubble_x: 44.375,
+        bubble_y: 301.6666666666667,
+        lip_x: 142.13028169014086,
+        lip_y: 208.79629629629628,
+        tail_deg: -43.532078140890356,
+        tail_offset_x: 57.99910950699644,
+        tail_offset_y: -55.10084660325497,
+      },
+      {
+        dialog: "I really love the company that I am working for.",
+        emotion: "normal",
+        bubble_offset_x: 44.375,
+        bubble_offset_y: 301.6666666666667,
+        bubble_x: 44.375,
+        bubble_y: 301.6666666666667,
+        lip_x: 140.9154929577465,
+        lip_y: 208.79629629629628,
+        tail_deg: -43.88994633164114,
+        tail_offset_x: 57.65382172309251,
+        tail_offset_y: -55.462030622055906,
+      },
+      {
+        dialog: "My co-workers are all really friendly and helpful.",
+        emotion: "normal",
+        bubble_offset_x: 44.375,
+        bubble_offset_y: 301.6666666666667,
+        bubble_x: 44.375,
+        bubble_y: 301.6666666666667,
+        lip_x: -1,
+        lip_y: -1,
+        tail_offset_x: null,
+        tail_offset_y: null,
+      },
+      {
+        dialog: "They really help me feel welcome.",
+        emotion: "normal",
+        bubble_offset_x: 44.375,
+        bubble_offset_y: 301.6666666666667,
+        bubble_x: 44.375,
+        bubble_y: 301.6666666666667,
+        lip_x: -1,
+        lip_y: -1,
+        tail_offset_x: null,
+        tail_offset_y: null,
+      },
+    ],
+  },
+];
